1c0d4e34928370aba766b9f7d8f10085b178e401
Sayaka <sayaka.ssk30@gmail.com>
2025-11-02 17:11:21 +0900

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Sayaka Sasaki on 2025/11/02.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
