917a567a9cd505e09a0c24aaab7a5888d817ee69
Sayaka <sayaka.ssk30@gmail.com>
2025-11-02 22:27:06 +0900

    身長と体重が連動してしまう（BMI未）

--- BMI/ContentView.swift
+++ BMI/ContentView.swift
@@ -1,4 +1,3 @@
-//
 //  ContentView.swift
 //  BMI
 //
@@ -8,13 +7,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enteredNumber: Double = 0.0
+    @State private var originalUnit = "meters"
+    
+    
+    let lengthUnits = ["meter", "centimeters"]
+    
     var body: some View {
         Form{
-            
+            //身長の入力欄
+            Section("入力") {
+                HStack {
+                    TextField("Original", value: $enteredNumber, format: .number)
+                        .keyboardType(.numberPad)
+                        .padding()
+                    
+                    Picker("", selection: $originalUnit) {
+                        ForEach(lengthUnits, id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                }
+                
+                //体重の入力欄（textField）
+                HStack {
+                    TextField("Original", value: $enteredNumber, format: .number)
+                        .keyboardType(.numberPad)
+                        .padding()
+                    
+                    Text("kg")
+                    
+                }
+            }
+            //BMI(text)
+            Section("結果"){
+                Text("Text 1")
+                
+                
+            }
         }
+        
     }
 }
-
-#Preview {
-    ContentView()
-}
+    
+    
+    
+    #Preview {
+        ContentView()
+    }
